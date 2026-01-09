616448826f48cf8912dbd8f5e9268dc9115c8a0e
Abhishek Bhardwaj <67678682+AbhishekStrix@users.noreply.github.com>
2026-01-10 03:15:59 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,12 +32,12 @@ app.get("/books", (req, res) => {
   });
 });
 
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("/{*any}", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 const startserver = async () => {
   await connectDb();
